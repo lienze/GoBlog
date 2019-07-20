4d83ff1eb960fd1fb41ecd9cd7834b40e0fe833c
Enze Li <lienze2010@126.com>
2019-07-20 13:47:42 +0800

    import new package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lienze/GoBlog
 
 go 1.12
+
+require github.com/lienze/go2db v0.0.0-20190720052107-fe99624cdeb3
--- src/main/main.go
+++ src/main/main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"github.com/lienze/go2db/dao"
 	"html/template"
+	"net/http"
 )
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("html/index.html")
 	t.Execute(w, "Hello World")
 }
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("html/login.html")
+	t.Execute(w, "")
+}
 
 func main() {
 	server := http.Server{
-		Addr:"10.0.2.15:8080",
+		Addr: "10.0.2.15:8080",
 	}
-	http.HandleFunc("/",rootPage)
+	http.HandleFunc("/", rootPage)
+	http.HandleFunc("/login", loginPage)
 	fmt.Println("GoBlog is running...")
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
+	dao.InitDB("mytest")
 }
-
