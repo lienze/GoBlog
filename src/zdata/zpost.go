474526f99c2afb7a679a77fe25310e36d59a20b0
Enze Li <lienze2010@126.com>
2019-08-14 09:15:09 +0800

    added zpost

---
+++ src/zdata/zpost.go
@@ -0,0 +1,11 @@
+package zdata
+
+type PostStruct struct {
+	PostPath       string
+	PostTitle      string
+	PostProfile    string
+	PostDate       string
+	PostContent    string
+	PostReadNum    int
+	PostCommentNum int
+}
