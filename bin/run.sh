92591a87b96bb25431a207d519b1e045664537ab
Enze Li <lienze2010@126.com>
2019-08-18 21:48:16 +0800

    added third party tool

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# update the js and css resource
+rm ./jscss/*.min.js
+java -jar ./the3party/yuicompressor-2.4.8.jar --type js --charset utf-8 -o '.js$:.min.js' ./html/js/*.js
+mv ./html/js/*.min.js ./public/js/
+
 go run ../main.go
