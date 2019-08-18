03327144c3767e65f8575a264248c73385a8bb4e
Enze Li <lienze2010@126.com>
2019-08-18 22:21:34 +0800

    update build and run shell scripts

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# update the js and css resource
-rm ./jscss/*.min.js
-java -jar ./the3party/yuicompressor-2.4.8.jar --type js --charset utf-8 -o '.js$:.min.js' ./html/js/*.js
-mv ./html/js/*.min.js ./public/js/
-
 go run ../main.go
