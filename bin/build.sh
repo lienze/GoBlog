385d28a7eb2343e4bcd00b7e2f73bbdaf4596787
Enze Li <lienze2010@126.com>
2019-08-19 20:31:53 +0800

    commit build.sh file

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update the js and css resource
-rm ./public/js/*.min.js
+# rm ./public/js/*.min.js
 java -jar ./the3party/yuicompressor-2.4.8.jar --type js --charset utf-8 -o '.js$:.min.js' ./html/js/*.js
 mv ./html/js/*.min.js ./public/js/
 
