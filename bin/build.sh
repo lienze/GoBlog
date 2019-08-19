1ae0f04aac46b0fca0d0778c3338a314fd6e1aa1
Enze Li <lienze2010@126.com>
2019-08-19 10:24:08 +0800

    added tips when build succeed

--- bin/build.sh
+++ bin/build.sh
@@ -6,3 +6,7 @@ java -jar ./the3party/yuicompressor-2.4.8.jar --type js --charset utf-8 -o '.js$
 mv ./html/js/*.min.js ./public/js/
 
 go build -gcflags "-N -l" -i -o ./main ../main.go
+
+if [ -f "./main" ];then
+	echo "build succeed!"
+fi
