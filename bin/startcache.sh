9bce5b4e73da45a2577c17203b8da44905eb3dd1
Enze Li <lienze2010@126.com>
2019-09-10 17:19:29 +0800

    record log when redis is running

--- bin/startcache.sh
+++ bin/startcache.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-./the3party/redis-server ./config/redis.conf &
+logname=$(date +%Y%m%d%k%M%S)
+if [ ! -d "./cache/log/" ];then
+	mkdir -p "./cache/log"
+fi
+echo $logname
+./the3party/redis-server ./config/redis.conf > ./cache/log/$logname 2>&1 &
 
