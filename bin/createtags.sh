6386c2a4df0aca301b0d23fadc4c099ed6349bdf
Enze Li <lienze2010@126.com>
2019-09-16 10:18:16 +0800

    added some note into createtags.sh

--- bin/createtags.sh
+++ bin/createtags.sh
@@ -7,14 +7,18 @@ for filename in $files
 do
 	if [[ ${filename:0:6} == "cscope" ]];then
 		rm cscope*
+		echo "remove cscope files..."
 		break
 	fi
 done
 
 if [ -f tags ];then
 	rm tags
+	echo "remove tags..."
 fi
 gotags -R ./ > tags
+echo "build tags..."
 find ./ -name "*.go" > ./cscope.files
 cscope -Rbq
+echo "build cscope..."
 echo "rebuild succeed!"
