1545a2729e1a405a4c64e33a4a61b1c055e2c833
Enze Li <lienze2010@126.com>
2019-09-23 10:16:31 +0800

    update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # go test ../... -cover -v 
-go test ../src/cache -cover -v
-go test ../src/zversion -cover -v
-go test ../src/ztime -cover -v
+
+# go test ../src/cache -cover -v
+# go test ../src/zversion -cover -v
+# go test ../src/ztime -cover -v
+
+package_list=(
+	../src/cache
+	../src/zversion
+	../src/ztime
+)
+for i in ${package_list[@]};
+do
+	echo $i
+	go test $i -cover -v
+done
 
