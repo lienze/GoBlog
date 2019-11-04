e31051574b6d295270a06e72d72a524755147e3b
Enze Li <lienze2010@126.com>
2019-11-04 13:08:40 +0800

    adjust the parameter of programm

--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-logname=$(date +%Y%m%d%k%M%S)
+logname=$(date+%Y%m%d%k%M%S)
 if [ ! -d "./log/"  ];then
 	mkdir -p "./log"
 fi
 
 #echo $1
-if [ $1x = "--debug"x ];then
-	echo "debug mode"
-	go run ../main.go
-else
+if [ $1x = "--release"x ];then
 	echo "release mode"
 	./GoBlog > ./log/$logname 2>&1 &
+else
+	echo "debug mode"
+	go run ../main.go
 fi
 
